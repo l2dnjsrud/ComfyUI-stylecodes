30e6d1733e93fc884a74103ebd2d2da87f310b44
Strawberries <crowles98@gmail.com>
2024-11-17 16:47:28 +0000

    improved single call functions

--- demo_make_stylecode.py
+++ demo_make_stylecode.py
@@ -8,9 +8,19 @@ from controlnet.controlnetxs_appearance import StyleCodesModel
 from diffusers.models import UNet2DConditionModel
 from transformers import AutoProcessor, SiglipVisionModel
 
-def process_single_image(image_path, prompt, num_inference_steps, output_folder,stylecode):
+def process_single_image(image_path, image=None):
+    
+    # Set up model components
+    unet = UNet2DConditionModel.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="unet", torch_dtype=torch.float16, device="cuda")
+    stylecodes_model = StyleCodesModel.from_unet(unet, size_ratio=1.0).to(dtype=torch.float16, device="cuda")
+    stylecodes_model.requires_grad_(False)
+    stylecodes_model= stylecodes_model.to("cuda")   
+
+
+    stylecodes_model.load_model("models/controlnet_model_11_80000.bin")
     # Load and preprocess image
-    image = Image.open(image_path).convert("RGB")
+    if image is None:
+        image = Image.open(image_path).convert("RGB")
     image = image.resize((512, 512))
 
     # Set up generator with a fixed seed for reproducibility
@@ -31,29 +41,16 @@ def process_single_image(image_path, prompt, num_inference_steps, output_folder,
 
 
 # Set up argument parsing
-parser = argparse.ArgumentParser()
-parser.add_argument("--image_path", type=str, default="test_images/vangouh.jpg", help="Path to the input image")
-parser.add_argument("--prompt", type=str, default="a man in a room", help="Prompt for image generation")
-parser.add_argument("--num_inference_steps", type=int, default=20, help="Number of inference steps")
-parser.add_argument("--output_folder", type=str, default="output", help="Path to save the output image")
-#stylecode
-parser.add_argument("--stylecode", type=str, default=r"Aj8dPwAACD8rECAlCC8UGzU/HwI", help="")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_path", type=str, default="test_images/vangouh.jpg", help="Path to the input image")
+
+    args = parser.parse_args()
+
+    # Ensure the output folder exists
+    if not os.path.exists(args.output_folder):
+        os.makedirs(args.output_folder)
 
 
-args = parser.parse_args()
-
-# Ensure the output folder exists
-if not os.path.exists(args.output_folder):
-    os.makedirs(args.output_folder)
-
-# Set up model components
-unet = UNet2DConditionModel.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="unet", torch_dtype=torch.float16, device="cuda")
-stylecodes_model = StyleCodesModel.from_unet(unet, size_ratio=1.0).to(dtype=torch.float16, device="cuda")
-stylecodes_model.requires_grad_(False)
-stylecodes_model= stylecodes_model.to("cuda")   
-
-
-stylecodes_model.load_model("models/controlnet_model_11_80000.bin")
-
-# Process a single image with the specified prompt
-process_single_image(args.image_path, args.prompt, args.num_inference_steps, args.output_folder,stylecode=args.stylecode)
+    # Process a single image with the specified prompt
+    print(process_single_image(args.image_path))
